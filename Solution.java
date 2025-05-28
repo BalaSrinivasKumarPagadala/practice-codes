729f39f41c08eaf5b0f6c4dfe2c9d9ef8ebe74d6
BalaSrinivasKumarPagadala <srinivaspbsk13@gmail.com>
2025-05-28 11:20:57 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,13 @@
+GCD of two numbers
+
+
+class Solution {
+    public static int gcd(int a, int b) {
+      while (b!=0){
+          int temp=a%b;
+          a=b;
+          b=temp;
+      }
+    return a;
+    }
+}
